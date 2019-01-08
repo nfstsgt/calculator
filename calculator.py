6fc9030ceb40a9978a62c357d42c8fa294badf34
nfstsgt <1465360864@qq.com>
2019-01-08 16:54:44 +0800

    提交计算器项目，两个文件代码内容相同

---
+++ calculator.py
@@ -0,0 +1,102 @@
+#声明两个用户输入的变量
+
+first_number = input("请输入第一个数字:")
+operation = input("请输入运算符:")
+second_number = input("请输入第二个数字:")
+
+
+
+#开发思路
+#两组变量进行运算，可分为四种情况，第一个变量为整数或者浮点数，第二个变量也可为整数或者浮点数，
+# 那么有整数和整数，整数和浮点数，浮点数和整数，浮点数和浮点数，
+# 因此要判断用户输入的两组变量数值中是否包含浮点数，没有则为整数类型
+# 用条件控制语句将其分别进行对应的转换再做运算操作，
+# 最后将结果result转换为字符串类型输出结果
+
+
+
+#根据有没有"." 判断用户输入数字类型，并进行字符串到数字类型转换
+if "." in first_number:
+    first_number = float(first_number)
+else:
+    first_number = int(first_number)
+
+if "." in second_number:
+    second_number = float(second_number)
+else:
+    second_number = int(second_number)
+
+#条件控制部分 根据用户输入的符号执行相应的计算及结果输出，同时对result进行数字到字符串的转换，
+# 将整数类型的result转换为字符串类型然后赋值给要输出的result
+#运行过程，利用 if条件语句，“==”，判断用户输入的运算符和下列哪一组运算过程的执行条件相符，执行相应的一组代码，
+#然后输出结果
+
+if operation == "+":
+    result = first_number + second_number
+    result = str(result)
+    print("加法运算结果="+result)
+
+elif operation == "-":
+    result = first_number - second_number
+    result = str(result)
+    print("减法运算结果="+result)
+
+elif operation == "*":
+    result = first_number * second_number
+    result = str(result)
+    print("乘法运算结果="+result)
+
+elif operation == "/":
+    result = first_number / second_number
+    result = str(result)
+    print("除法运算结果="+result)
+
+elif operation == "%":
+    result = first_number % second_number
+    result = str(result)
+    print("取模运算结果="+result)
+
+elif operation == "**":
+    result = first_number ** second_number
+    result = str(result)
+    print("幂运算结果="+result)
+
+elif operation == "//":
+    result = first_number // second_number
+    result = str(result)
+    print("取整除运算结果="+result)
+
+elif operation == "+=":
+    first_number += second_number
+    result = str(first_number)
+    print("加等于运算结果="+result)
+
+elif operation == "-=":
+    first_number -= second_number
+    result = str(first_number)
+    print("减等于运算结果="+result)
+
+elif operation == "*=":
+    first_number *= second_number
+    result = str(first_number)
+    print("乘等于运算结果="+result)
+
+elif operation == "/=":
+    first_number /= second_number
+    result = str(first_number)
+    print("第一个数字取 除等于运算结果="+result)
+
+elif operation == "%=":
+    first_number %= second_number
+    result = str(first_number)
+    print("第一个数字取 模等于运算结果="+result)
+
+elif operation == "**=":
+    first_number **= second_number
+    result = str(first_number)
+    print("第一个数字取 幂等于运算结果="+result)
+
+elif operation == "//=":
+    first_number //= second_number
+    result = str(first_number)
+    print("第一个数字取 整除等于运算结果="+result)
